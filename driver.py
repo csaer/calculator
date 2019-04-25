afed273d4d710360e23fa1a92a91697852740cd7
AlexAtPSU <ajw29@pdx.edu>
2019-04-25 11:04:14 -0700

    driver requires command line input
    first argument [1] is the input for the factorial function

--- driver.py
+++ driver.py
@@ -3,8 +3,14 @@
 import sys
 import factorial as f
 
+def usage():
+	print("This requires 1 argument.\nThe given argument should be a number.")
+
 def main():
-    print('10! = ' + str(f.factorial(10)))
+    print(sys.argv[1] +'! = ' + str(f.factorial(float(sys.argv[1]))))
 
 if __name__ == '__main__':
-    main()
+	if len(sys.argv) > 1 and type(float(sys.argv[1])) == type(1.1):
+		main()
+	else:
+		usage()
