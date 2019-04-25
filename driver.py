bdb4eed8ce420e5bbd442d7e23ef66f7e6e79abe
kzalewski11 <49792318+kzalewski11@users.noreply.github.com>
2019-04-25 11:01:21 -0700

    Added tests for subtract and multiply
    check it out check it out check it out check it out check it out check it out

--- driver.py
+++ driver.py
@@ -2,9 +2,13 @@
 
 import sys
 import factorial as f
+import subtract as s
+import mult as m
 
 def main():
     print('10! = ' + str(f.factorial(10)))
+    print('10 - 5 = ' + str(s.subtract(10,5)))
+    print('10 * 50 = ' + str(m.multiply(10,50)))
 
 if __name__ == '__main__':
     main()
