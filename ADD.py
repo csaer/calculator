bcd71ea2581b19b8dd9a2987a1f4943db321ad07
Dom26 <50036427+Dom26@users.noreply.github.com>
2019-04-26 19:34:19 -0700

    Working on ADD

--- ADD.py
+++ ADD.py
@@ -1,8 +1,8 @@
 def ADD(num1,num2):
-    i = 0;
-    count = 0;
-    sum = num1;
-    while(count < num2):
-        sum += 1;
-        count += 1;
-    return sum;
+    i = 0
+    sum = num1
+    while(i < num2):
+        sum += 1
+        i += 1
+    return sum
+
--- driver.py
+++ driver.py
@@ -4,11 +4,13 @@ import sys
 import factorial as f
 import subtract as s
 import mult as m
+import ADD as add
 
 def main():
     print('10! = ' + str(f.factorial(10)))
     print('10 - 5 = ' + str(s.subtract(10,5)))
     print('10 * 50 = ' + str(m.multiply(10,50)))
+    print('2 + 3 = ' + str(add.ADD(2,3)))
 
 if __name__ == '__main__':
     main()
