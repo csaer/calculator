4b6b8b027e522e02a99d8dda2b0a98f68a4e25c8
kzalewski11 <49792318+kzalewski11@users.noreply.github.com>
2019-05-03 13:08:29 -0700

    Removed extra comment lines/old test code

--- factorial.py
+++ factorial.py
@@ -14,14 +14,7 @@ def factorial( n ):
 
 		#while loop to iteratively calculate factorial
 		while m > 0:
-			#uncomment line 16 when multiplication function has been completed
-			#rename function call as appropriate
-			#comment out line 17
 			n = multiply(n, m)
-			#n *= m
-			#uncomment line 21 when subtraction function has been completed
-			#rename/modify function call as appropriate
-			#comment out line 22
 			m = subtract(m,1)
 
 		#return calculated factorial
